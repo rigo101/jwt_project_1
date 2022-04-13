7868bc2648bc3eb2ce91bb20d7f20b09fd66ec3c
Rigo Ildiko <ildiko.bcb@gmail.com>
2022-04-13 21:14:39 +0200

    Receive and validate user input

--- app.js
+++ app.js
@@ -16,9 +16,24 @@ app.get("/", (req, res) => {
 });
 
 // Register - URL: http://localhost:3000/register
+// {"first_name": "Waltz", "last_name": "Udo", "email": "waltz@info.com", "password": "mypassword"}
+    
 app.post("/register", (req, res) => {
-    console.log(req.body);
-    res.status(200).send("OK");
+    try {
+        // throw new Error('Fityiszt neked');
+        // Get user input    
+        const { first_name, last_name, email, password } = req.body;
+        console.log(first_name, last_name, email, password);
+        // Validate user input
+        if (!(email && password && first_name && last_name)) {
+            res.status(400).send("All input is required");
+        } else {
+            res.status(200).send("OK");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
 });
 
 // Login
