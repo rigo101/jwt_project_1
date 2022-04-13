0fbb0cadc765f11826df5421cefa41c7a2acdbc3
Rigo Ildiko <ildiko.bcb@gmail.com>
2022-04-13 22:23:13 +0200

    User registration: if registered, do not duplicate user create encrypted password create user create token

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 require("./config/database").connect();
 const express = require("express");
-
 const app = express();
 
 app.use(express.json()); // JSON middleware - it fullfils req.body with JS object
@@ -18,7 +19,7 @@ app.get("/", (req, res) => {
 // Register - URL: http://localhost:3000/register
 // {"first_name": "Waltz", "last_name": "Udo", "email": "waltz@info.com", "password": "mypassword"}
     
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
     try {
         // throw new Error('Fityiszt neked');
         // Get user input    
@@ -26,10 +27,38 @@ app.post("/register", (req, res) => {
         console.log(first_name, last_name, email, password);
         // Validate user input
         if (!(email && password && first_name && last_name)) {
-            res.status(400).send("All input is required");
-        } else {
-            res.status(200).send("OK");
+            return res.status(400).send("All input is required");
+        } 
+        // check if user already exist
+        // Validate if user exist in our database
+        const oldUser = await User.findOne({ email });  
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
         }
+
+        //Encrypt user password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // Create user in our database
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+
+        // Create token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            { expiresIn: "2h" }   
+        );
+        // save user token
+        user.token = token;
+    
+        // return new user
+        res.status(201).json(user); // 201 Created
     } catch (error) {
         console.log(error);
         res.status(500).send(error.message);
